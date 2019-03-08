64e9a5c65301df3cbad681ea5a80f122c7f175f4
weini <weini@qq.com>
2019-03-08 21:04:38 +0800

    修改User

--- MyGit/src/main/java/com/mycode/domian/User.java
+++ MyGit/src/main/java/com/mycode/domian/User.java
@@ -3,4 +3,20 @@ package com.mycode.domian;
 public class User {
     private Integer age;
     private String name;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
